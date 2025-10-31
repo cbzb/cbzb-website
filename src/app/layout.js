c3f36144a0d35f5661676de3a727884a9e2c5993
cbzb <cbzb.tech@gmail.com>
2025-10-31 10:40:27 -0300

    GTM

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Playfair_Display, Montserrat } from 'next/font/google'
+import Script from 'next/script'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 
@@ -15,6 +16,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR" className={`${playfair.variable} ${montserrat.variable}`}>
       <body className="min-h-dvh bg-primary-dark text-text-light">
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WQGQLMZ3');`}
+        </Script>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WQGQLMZ3"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Header />
         <main>{children}</main>
         <Footer />
